182b8755dc968299b20d7bf9c242564289df21b0
copoet <copoet@126.com>
2024-01-20 20:10:22 +0800

    init add base model

---
+++ internal/model/models.go
@@ -0,0 +1,28 @@
+package model
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type BaseModel struct {
+	ID        uint `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Create 在数据库中创建记录
+func (base *BaseModel) Create(db *gorm.DB) error {
+	return db.Create(base).Error
+}
+
+// Update 更新数据库中的记录
+func (base *BaseModel) Update(db *gorm.DB, values interface{}) error {
+	return db.Model(base).Updates(values).Error
+}
+
+// Delete 从数据库中删除记录
+func (base *BaseModel) Delete(db *gorm.DB) error {
+	return db.Delete(base).Error
+}
+
