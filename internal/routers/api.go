1a470fe76c8d09b0fac6144d09b2e04fe813de0d
copoet <copoet@126.com>
2024-01-24 18:22:39 +0800

    adjust middleware

--- internal/routers/api.go
+++ internal/routers/api.go
@@ -2,11 +2,14 @@ package routers
 
 import (
 	controllers "gin-base/internal/controller"
+	"gin-base/internal/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter(router *gin.Engine) {
 	userController := new(controllers.UserController)
 	//router.POST("/users", userController.Create)
+	//测试中间价
+	router.Use(middleware.LoggerMiddleware())
 	router.GET("/users/list", userController.GetList)
 }
