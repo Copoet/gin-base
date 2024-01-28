085e51a0cdddcbcfb98fb1373532e2822c1c2816
copoet <copoet@126.com>
2024-01-28 21:08:31 +0800

    add jwt

--- internal/controller/auth_contorller.go
+++ internal/controller/auth_contorller.go
@@ -1,1 +1,28 @@
 package controllers
+
+import (
+	"gin-base/pkg/util"
+	"github.com/gin-gonic/gin"
+)
+
+type AuthController struct {
+	BaseController
+}
+
+func (a *AuthController) Login(c *gin.Context) {
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
+	// 1.参数校验
+	if username == "" || password == "" {
+		a.ReturnFail(c, util.PublicParamsIllegal, nil)
+		return
+	}
+
+	// 2.根据用户名和密码去数据库查询
+	// 3.返回响应
+	// 4.登录成功后,生成token
+	// 5.返回token
+	// 6.返回响应
+
+}
