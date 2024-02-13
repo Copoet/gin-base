42b7503d31b8280e3e963b5cc596d7aaa761373d
copoet <copoet@126.com>
2024-02-13 22:35:43 +0800

    add model

--- internal/model/role_permission.go
+++ internal/model/role_permission.go
@@ -2,21 +2,6 @@ package model
 
 import "gorm.io/gorm"
 
-/*
-*
-CREATE TABLE `auht_role_permission` (
-
-	`id` int NOT NULL AUTO_INCREMENT,
-	`role_id` int NOT NULL COMMENT '角色ID',
-	`permisson_id` int DEFAULT NULL COMMENT '权限ID',
-	`status` tinyint(1) DEFAULT NULL COMMENT '是否启用 1是2否',
-	`is_delete` tinyint(1) DEFAULT NULL COMMENT '是否删除 1 是 2 否',
-	`create_time` datetime DEFAULT NULL COMMENT '创建时间',
-	`update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-	PRIMARY KEY (`id`) USING BTREE
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='角色权限关联表';
-*/
 type RolePermission struct {
 	BaseModel
 	ID           int `json:"id"`
